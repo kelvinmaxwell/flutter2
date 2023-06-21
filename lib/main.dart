8d12421eadd72ee67e3aca1a1073c3f96fb68f6b
kelvinmaxwell <kelvinmaxwell82@gmail.com>
2023-06-22 02:33:57 +0300

    flut

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main()=>runApp(MaterialApp(
-  home: Text('hey ninjas!'),
+  home: Home()
+
 ));
+
+
+class Home extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body:Row(
+        children: <Widget>[
+          Expanded(child: Image.asset('Assets/studio.png'),
+          flex:3),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.cyan,
+              child: Text("1"),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(padding: EdgeInsets.all(30.0),
+              color: Colors.pink,
+              child: Text("2"),),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(padding: EdgeInsets.all(30.0),
+              color: Colors.amber,
+              child: Text("3"),),
+          ),
+
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      )
+      ,
+    );
+  }
+}
+
 
